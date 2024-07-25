c6b0bf100410c17c91dd3eb8268f141ac0e39b78
Jesse Maitland <42497478+JesseMaitland@users.noreply.github.com>
2024-07-25 13:16:29 +0200

    drop python 3.8

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=['3.8', '3.9', '3.10', '3.11', '3.12'])
+@nox.session(python=['3.9', '3.10', '3.11', '3.12'])
 def tests(session):
     requirements = nox.project.load_toml("pyproject.toml")["project"]["dependencies"]
     session.install(*requirements)
