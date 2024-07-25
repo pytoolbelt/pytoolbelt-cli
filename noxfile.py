6bec5cf5ca79c53efd44899edaf4b43abb92572f
Jesse Maitland <42497478+JesseMaitland@users.noreply.github.com>
2024-07-25 13:28:13 +0200

    add formatting to nox

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,14 @@
 import nox
 
 
+@nox.session
+def check_formating(session):
+    requirements = nox.project.load_toml("pyproject.toml")["project"]["optional-dependencies"]["fmt"]
+    session.install(*requirements)
+    session.run("black", "--check", "pytoolbelt", "tests")
+    session.run("isort", "--check-only", "pytoolbelt", "tests")
+
+
 @nox.session(python=['3.9', '3.10', '3.11', '3.12'])
 def tests(session):
     requirements = nox.project.load_toml("pyproject.toml")["project"]["dependencies"]
