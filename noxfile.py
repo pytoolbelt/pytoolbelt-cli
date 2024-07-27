5f996d470c85dc3a9dc67634730e3e2f6e64de87
Jesse Maitland <42497478+JesseMaitland@users.noreply.github.com>
2024-07-27 20:02:04 +0200

    update nox and toml

--- noxfile.py
+++ noxfile.py
@@ -7,8 +7,8 @@ def check_formating(session):
         "optional-dependencies"
     ]["fmt"]
     session.install(*requirements)
-    session.run("black", "--check", "pytoolbelt", "tests")
-    session.run("isort", "--check-only", "pytoolbelt", "tests")
+    session.run("ruff", "check", "pytoolbelt", "tests")
+    session.run("ruff", "check", "pytoolbelt", "tests", "--select", "I")
 
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12"])
